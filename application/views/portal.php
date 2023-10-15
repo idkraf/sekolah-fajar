c52c7473e6383a0659b1d307f1edf3175b1de852
wanss <mohbaswandegorie@gmail.com>
2023-10-16 06:22:14 +0700

    first

---
+++ application/views/portal.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<title>SMPTQ AL IKHLAS</title>
+	<link rel="icon" type="image/png" href="<?php echo media_url('img/smplogo.png') ?>">
+
+	<!-- Bootstrap Core CSS -->
+	<link href="<?php echo media_url() ?>/css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="<?php echo media_url() ?>/css/load-font-googleapis.css">
+	<!-- Font Awesome -->
+	<link rel="stylesheet" href="<?php echo media_url() ?>/css/font-awesome.min.css">
+
+
+	<!-- Custom CSS -->
+	<link href="<?php echo media_url() ?>/css/frontend-style.css" rel="stylesheet">
+	<link href="<?php echo media_url() ?>/css/portal.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+	<!-- Home -->
+<body style="background-image:url(asset/images/icon/bg4.png);background-size:cover;">
+    <div class="page-wrapper">
+        <div class="page-content--bge5">
+            <div class="container">
+                <div class="login-wrap">
+                    <div class="login-content">
+	<section class="content-section">
+		<div class="container text-center">
+			<div class="row">
+				<div class="col-md-12">
+					<h3> SISTEM INFORMASI PEMBAYARAN </h3>
+					<p class="lead mb-5 colr"><h1><strong>SMPTQ AL IKHLAS BREBES</strong></h1></p>
+				</div>	
+				
+				<div class="col-md-4">
+					<a href="<?php echo site_url('manage') ?>">
+					<div class="box">
+						<i class="fa fa-user icon-menu"></i>
+						<br>
+						<strong>Login Admin</strong> 
+					</div>
+				</a>
+				</div>
+				<div class="col-md-4">
+					<a href="<?php echo site_url('home') ?>">
+					<div class="box">
+						<i class="fa fa-money icon-menu"></i>
+						<br>
+						<strong>Cek Pembayaran</strong>
+					</div>
+				</a>
+				</div>
+				<div class="col-md-4">
+					<a href="<?php echo site_url('student') ?>">
+					<div class="box">
+						<i class="fa fa-graduation-cap icon-menu"></i>
+						<br>
+						<strong>Login Siswa</strong>
+					</div>
+				</a>
+				</div>
+			</div>
+		</div>
+	</section>
+
+
+</body>
+
+</html>
